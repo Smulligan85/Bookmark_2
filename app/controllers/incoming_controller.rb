0f397868515ecc0444d4c50dcdc491e5848f6bcd
Sean Mulligan <sean.mulligan85@gmail.com>
2015-04-19 16:34:25 -0700

    Updating incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,27 +9,26 @@ class IncomingController < ApplicationController
     puts "INCOMING PARAMS HERE: #{params}"
 
     # Find the user by using params[:sender]
-    user = User.find_by(email: params[:sender])
+    @user = User.find_by(email: params[:sender])
      
     # Find the topic by using params[:subject]
-    topic = Topic.find_by(email: params[:subject])
+    @topic = Topic.find_by(title: params[:subject])
 
     # Assign the url to a variable after retreiving it from params["body-plain"]
-    url = params["body-plain"]
+    @url = params["body-plain"]
 
     # Check if user is nil, if so, create and save a new user
-    if user.nil?
+    if @user.nil?
       return head 500
     end
 
     # Check if the topic is nil, if so, create and save a new topic
-    if topic.nil?
-      topic = Topic.new(title: params[:subject])
-      topic.save
+    if @topic.nil?
+      topic = @user.topics.create(title: params[:subject])
     end
     
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
-    bookmark = Bookmark.build(title: params["body-plain"])
+    @bookmark = @topic.bookmarks.create(url: @url)
     if bookmark.save
     # Assuming all went well. 
     head 200
